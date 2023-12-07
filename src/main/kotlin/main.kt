3181f1e27a150188f993d955b3c554f6ccd6fc25
Mhun9799 <muhun1030@gmail.com>
2023-12-07 19:46:58 +0900

    readLine() -> readln() 변경

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,11 +9,11 @@ fun main() {
     val select: Int? = readLine()?.toInt()
 
     print("첫번째 숫자를 입력 해주세요: ")
-    val x: Int? = readLine()?.toInt()
+    val x: Int = readln().toInt()
     print("두번째 숫자를 입력 해주세요: ")
-    val y: Int? = readLine()?.toInt()
+    val y: Int = readln().toInt()
 
-    if (x != null && y != null) {
+   // if (x != null && y != null) {
         when (select) {
             1 -> println("$x 더하기 ${y}의 결과는 : ${calc.addOperation(AddOperation(), x, y)} 입니다.")
             2 -> println("$x  빼기 ${y}의 결과는  ${calc.substractOperation(SubstractOperation(), x, y)} 입니다.")
@@ -21,8 +21,8 @@ fun main() {
             4 -> println("$x 나누기 ${y}의 결과는 ${calc.divideOperation(DivideOperation(), x, y)} 입니다.")
 
         }
-    } else{
-        println("올바른 값을 입력 해주세요!!")
-    }
+   // } else{
+    //    println("올바른 값을 입력 해주세요!!")
+   // }
 
 }
